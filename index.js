4e36e7fa4a2c375c8de65d98d306d8569024a5fa
Dillon Mulroy <dillon.mulroy@gmail.com>
2018-11-23 23:54:10 -0500

    add cluster module

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+
 const server = require('./src/server');
 
-server
-  .listen(3000)
-  .then(({ url }) => console.log(`ynab-graphql running at ${url}`))
-  .catch(err => console.log('error starting server', err));
+if (cluster.isMaster) {
+  console.log(`ynab-graphql master (pid: ${process.pid}) started...`);
+
+  for (let idx = 0; idx < numCPUs; idx++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', worker => {
+    console.log(`ynab-graphql worker (pid: ${worker.process.pid}) died`);
+    console.log('Forking a new process...');
+
+    cluster.fork();
+  });
+} else {
+  server
+    .listen(3000)
+    .then(() =>
+      console.log(`ynab-graphql worker (pid: ${process.pid}) started...`)
+    )
+    .catch(err => console.log('error starting server', err));
+}
